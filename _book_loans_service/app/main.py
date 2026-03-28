1cb0d9b327250f6d4794dba67fb14eebc30ca80b
Klaus BONOU SELEGBE <klaus.bonou.selgbe01@gmail.com>
2026-03-28 22:01:11 +0100

    Fix : Add cors middleware for allowing frontend

--- _book_loans_service/app/main.py
+++ _book_loans_service/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from .routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Book Loans Service",
     description="Microservice de gestion des emprunts - DIT Library",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
